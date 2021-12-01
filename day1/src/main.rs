675a6420c056888e00bf2868630fe6dc323810e5
Arjen Laarhoven <arjen@laarhoven.info>
2021-12-01 12:16:03 +0100

    Day1, refactor

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,21 +1,23 @@
 use std::io::{self, BufRead};
 
 fn main() {
-    let stdin = io::stdin();
-    let stdin = stdin.lock();
-
-    let measurements: Vec<u32> = stdin
+    let measurements: Vec<u32> = io::stdin()
+        .lock()
         .lines()
         .flatten()
         .flat_map(|v| v.parse::<u32>())
         .collect();
 
-    let answer1 = measurements.windows(2).filter(|s| s[1] > s[0]).count();
+    let answer1 = measure(&measurements, 2);
     println!("Measurements larger than previous measurement: {}", answer1);
 
-    let answer2 = measurements
-        .windows(4)
-        .filter(|s| s[1..4].iter().sum::<u32>() > s[0..3].iter().sum())
-        .count();
+    let answer2 = measure(&measurements, 4);
     println!("Sums larger than previous sum: {}", answer2);
+}
+
+fn measure(input: &[u32], window: usize) -> usize {
+    input
+        .windows(window)
+        .filter(|s| s[1..window].iter().sum::<u32>() > s[0..window - 1].iter().sum())
+        .count()
 }
