a0c7bba09ea0e886b9ceb0b15ec687e32aa6570b
Arjen Laarhoven <arjen@laarhoven.info>
2021-12-05 15:30:28 +0100

    Day 5, part 2 completed

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -1,36 +1,57 @@
 use regex::Regex;
 use lazy_static::lazy_static;
-use std::{io::{self, BufRead}, collections::HashMap};
+use std::{io::{self, Read}, collections::HashMap};
 
-fn main() -> Result<(), io::Error> {
+fn solution(input: &str, include_part2: bool) -> usize {
     lazy_static!{
         static ref RE: Regex = Regex::new(r"^(?P<x1>[\d]+),(?P<y1>[\d]+) -> (?P<x2>[\d]+),(?P<y2>[\d]+)$").unwrap();
     }
+    let mut coords: HashMap<(i32, i32), u32> = HashMap::new();
 
-    let mut coords: HashMap<(u32, u32), u32> = HashMap::new();
-    let stdin = io::stdin();
-    for line in stdin.lock().lines() {
-        let line = line.unwrap();
-        let caps = RE.captures(&line).unwrap();
-        let x1 = caps.name("x1").unwrap().as_str().parse::<u32>().unwrap();
-        let y1 = caps.name("y1").unwrap().as_str().parse::<u32>().unwrap();
-        let x2 = caps.name("x2").unwrap().as_str().parse::<u32>().unwrap();
-        let y2 = caps.name("y2").unwrap().as_str().parse::<u32>().unwrap();
-        if y1 == y2 {
-            for x in u32::min(x1, x2)..=u32::max(x1, x2) {
+    for line in input.lines() {
+        let caps = RE.captures(line).unwrap();
+        let value = |name| caps.name(name).unwrap().as_str().parse::<i32>().unwrap();
+        let x1 = value("x1");
+        let y1 = value("y1");
+        let x2 = value("x2");
+        let y2 = value("y2");
+
+        if y1 == y2 { // horizontal
+            for x in i32::min(x1, x2)..=i32::max(x1, x2) {
                 let r = coords.entry((x, y1)).or_insert(0);
                 *r += 1
             }
         }
-        if x1 == x2 {
-            for y in u32::min(y1, y2)..=u32::max(y1, y2) {
+        else if x1 == x2 { // vertical
+            for y in i32::min(y1, y2)..=i32::max(y1, y2) {
                 let r = coords.entry((x1, y)).or_insert(0);
                 *r += 1
             }
         }
+        else { // diagonal
+            if include_part2 {
+                let dx = if x1 < x2 { 1 } else { -1 };
+                let dy = if y1 < y2 { 1 } else { -1 };
+                let mut coord = (x1, y1);
+
+                for _ in i32::min(x1, x2)..=i32::max(x1, x2) {
+                    let r = coords.entry(coord).or_insert(0);
+                    *r += 1;
+                    coord = (coord.0 + dx, coord.1 + dy);
+                }
+            }
+        }
     }
-    let part1 = coords.iter().filter(|(_, &v)| v > 1).count();
-    println!("Number of overlapping points > 1: {}", part1);
+
+    coords.iter().filter(|(_, &v)| v > 1).count()
+}
+fn main() -> Result<(), io::Error> {
+    let mut stdin = io::stdin();
+    let mut input = String::new();
+    stdin.read_to_string(&mut input)?;
+
+    println!("Number of overlapping points (horizontal/vertial) > 1: {}", solution(&input, false));
+    println!("Number of overlapping points (horizontal/vertial/diagonal) > 1: {}", solution(&input, true));
 
     Ok(())
 }
